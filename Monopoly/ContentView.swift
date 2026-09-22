4ea69e369b782bc2fc7095b3b7353327d823a72c
Brian <brian2.0gameryt@gmail.com>
2026-09-21 22:07:23 -0400

    Initial Commit

---
+++ Monopoly/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Monopoly
+//
+//  Created by Brian on 9/21/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Monopoly/MonopolyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonopolyApp.swift
+//  Monopoly
+//
+//  Created by Brian on 9/21/26.
+//
+
+import SwiftUI
+
+@main
+struct MonopolyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
